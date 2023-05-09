9f9b6c4118f89ca8e18d11035241e72ff873a7ab
Christoph Pirkl <christoph.pirkl@exasol.com>
2023-05-09 11:30:18 +0200

    Revert "Use driver manager for installing JDBC driver"
    This reverts commit 2c84ce6b0183ae8d9c4fdd457fc5fcb92d9b9bee.

--- src/test/java/com/exasol/adapter/dialects/db2/IntegrationTestConfiguration.java
+++ src/test/java/com/exasol/adapter/dialects/db2/IntegrationTestConfiguration.java
@@ -11,6 +11,7 @@ public class IntegrationTestConfiguration {
     public static final String VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION = "virtual-schema-dist-10.5.0-db2-2.1.1.jar";
     public static final Path PATH_TO_VIRTUAL_SCHEMAS_JAR = Path.of("target", VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION);
     public static final int DB2_PORT = 50000;
+    public static final String JDBC_DRIVER_CONFIGURATION_FILE_NAME = "settings.cfg";
     public static final String JDBC_DRIVER_NAME = "jcc.jar";
     public static final Path JDBC_DRIVER_PATH = Path.of("target", "db2-driver", JDBC_DRIVER_NAME);
 
