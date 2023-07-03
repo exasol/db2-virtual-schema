64628c9524f8e9f6c8c068a660538afa726a1016
Christoph Kuhnke <github@kuhnke.net>
2023-07-03 16:07:12 +0200

    updated dependencies (#27)

--- src/test/java/com/exasol/adapter/dialects/db2/IntegrationTestConfiguration.java
+++ src/test/java/com/exasol/adapter/dialects/db2/IntegrationTestConfiguration.java
@@ -8,7 +8,7 @@ public class IntegrationTestConfiguration {
     public static final String EXASOL_DOCKER_REFERENCE = "7.1.19";
     public static final DockerImageName DB2_DOCKER_REFERENCE = DockerImageName
             .parse("icr.io/db2_community/db2:11.5.8.0").asCompatibleSubstituteFor("ibmcom/db2");
-    public static final String VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION = "virtual-schema-dist-10.5.0-db2-2.1.1.jar";
+    public static final String VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION = "virtual-schema-dist-10.5.0-db2-2.1.2.jar";
     public static final Path PATH_TO_VIRTUAL_SCHEMAS_JAR = Path.of("target", VIRTUAL_SCHEMAS_JAR_NAME_AND_VERSION);
     public static final int DB2_PORT = 50000;
     public static final String JDBC_DRIVER_CONFIGURATION_FILE_NAME = "settings.cfg";
